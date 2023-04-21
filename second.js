b513163cfde62119baa027086cc7c37c3628d04e
Hmj2209 <jagtapharshada01@gmail.com>
2023-04-21 22:39:04 +0530

    file crated

---
+++ second.js
@@ -0,0 +1,23 @@
+function print_natural_num (end){
+
+    for(let i =0;i<=end; i++){
+
+        console.log(i)
+
+    }
+
+}
+
+
+
+print_even_no(end)
+{
+    for(let i =0;i<=end; i++){
+
+        console.log(i)
+
+    }
+}
+
+
+let a="yuiihiuh";
